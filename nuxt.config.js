c98bd4d165c9a9b484f72d8e5a5f3e5f63a66a18
no5no6 <work_yuanyang@163.com>
2021-01-16 22:37:00 +0800

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  router: {
+    extendRoutes(routes, resolve) {
+      routes.splice(0)
+
+      routes.push(
+        ...[
+          {
+            path: '/',
+            component: resolve(__dirname, 'pages/layout/'),
+            children: [
+              {
+                path: '',
+                name: 'home',
+                component: resolve(__dirname, 'pages/home/')
+              },
+              {
+                path: 'login',
+                name: 'login',
+                component: resolve(__dirname, 'pages/login/')
+              },
+              {
+                path: 'register',
+                name: 'register',
+                component: resolve(__dirname, 'pages/login/')
+              }
+            ]
+          }
+        ]
+      )
+    }
+  }
+}
