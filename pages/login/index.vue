da34b66a6bc4a225c7421e692ac3f32c8a40d6d0
no5no6 <work_yuanyang@163.com>
2021-01-17 00:12:44 +0800

    完成静态页面

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -3,9 +3,9 @@
     <div class="container page">
       <div class="row">
         <div class="col-md-6 offset-md-3 col-xs-12">
-          <h1 class="text-xs-center">Sign up</h1>
+          <h1 class="text-xs-center">{{ isLogin ? 'Sign in' : 'Sign up' }}</h1>
           <p class="text-xs-center">
-            <a href="">Have an account?</a>
+            <a href="">{{ isLogin ? 'Need an account?' : 'Have an account?'}}</a>
           </p>
 
           <ul class="error-messages">
@@ -13,7 +13,7 @@
           </ul>
 
           <form>
-            <fieldset class="form-group">
+            <fieldset v-if="!isLogin" class="form-group">
               <input
                 class="form-control form-control-lg"
                 type="text"
@@ -35,7 +35,7 @@
               />
             </fieldset>
             <button class="btn btn-lg btn-primary pull-xs-right">
-              Sign up
+              {{ isLogin ? 'Sign in' : 'Sign up'}} 
             </button>
           </form>
         </div>
